51023387216d3b86fb384c3db9e5271f34fcda1b
josephcauchingp <joseph.cauchi@ngpems.com.mt>
2021-04-23 16:41:18 +0200

    add questions

---
+++ go.mod
@@ -0,0 +1,8 @@
+module quiz.com/quizcli
+
+go 1.16
+
+require (
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+)
--- quizcli/cmd/root.go
+++ quizcli/cmd/root.go
@@ -29,7 +29,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "questions", //"dadjoke",
+	Use:   "quiz", //"dadjoke",
 	Short: "Get all question Ids",
 	Long:  `Get all question Ids available for user to answer`,
 }
